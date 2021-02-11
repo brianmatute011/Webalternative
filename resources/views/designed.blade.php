13605be59655b0eefca7de5599d142498b671165
unknown <brianmatute011@gmail.com>
2021-02-11 12:11:14 -0800

    Starting Laravel Project

---
+++ resources/views/designed.blade.php
@@ -0,0 +1,170 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel ="stylesheet" href="vendor/css/bootstrap.min.css">
+    <link rel ="stylesheet" href="vendor/css/bootstrap-utilities.min.css">
+    <link rel="stylesheet" href="vendor/recourses_wa.css">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans:wght@400;700&display=swap" rel="stylesheet">
+    <title>WebAlternative</title>
+</head>
+<body>
+<nav class="navbar  bg-light_wa">
+    <h6 class="navbar_wa">LITSEN </h6>
+    <form >
+        <div >
+            <label class="nav_link_wa" for="textbox1"> Search</label>
+            <input type="text" class="form-text text_box_style" id="textbox1">
+        </div>
+    </form>
+    <ul class="nav nav-tabs  justify-content-lg-end">
+        <li class="nav-item" >
+            <a class="nav-link  nav_link_wa" href="#"> Live </a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link nav_link_wa"   href="#"> Album </a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link nav_link_wa"  href="#"> Events</a>
+        </li>
+        <li class="nav-item">
+            <img src="wa_icon/menu.png" alt = "menu icon" class="icon_resize_menu">
+        </li>
+    </ul>
+</nav>
+<section class="d-flex container-fluid container_fluid_style ">
+    <div class="row container-fluid justify-content-center">
+    <div class="col mx-3">
+        <p class="font_resize_title"><h6>Back</h6></p>
+        <img src="wa_icon/arrow-left.png" alt="arrow left icon" class="icon_resize">
+    </div>
+    <div class="col  justify-content-center align-items-center mx-5">
+        <p class="font_resize_title"> <h6>Category</h6></p>
+        <h1 class="font_resize">  ALTERNATIVE </h1>
+    </div>
+    <div class="col  justify-content-center align-items-center mx-5">
+        <p class="font_resize_title"> <h6>Artist</h6></p>
+        <h1 class="font_resize">580</h1>
+    </div>
+    <div class="col col-sm col-lg pt-4 mx-5">
+        <img src="wa_icon/arrow_down.png" alt = "arrow down icon" class="icon_resize">
+    </div>
+    </div>
+</section>
+<section class="container-fluid">
+    <div class="row">
+        <section class="col nav_link_wa_left">
+            <div class="row container-fluid div_class_style_6">
+                <div class="col div_class_style_circle">
+                    <img src="wa_icon/circle_fill_black.png" alt="circle_fill_black.png" class="icon_resize_circle_fill_black">
+                </div>
+                <div class="col">
+                    <h5> Alternative </h5>
+                    <h5> Inside</h5>
+                    <h5> Punk</h5>
+                </div>
+            </div>
+            <div class="container-fluid div_class_style_5">
+                <svg height="200" width="200">
+                    <circle cx="100" cy="100" r="60" stroke="black" stroke-width="1" fill="white" />
+                    <text fill="black" font-size="16" font-family="Verdana" x="70" y="95">Similar</text>
+                    <text fill="black" font-size="16" font-family="Verdana" x="70" y="115">Artist</text>
+                </svg>
+            </div>
+            <div class="row container-fluid div_class_style_6">
+                <div class="col div_class_style_circle">
+                    <img src="wa_icon/circle_fill_black.png" alt="circle_fill_black.png" class="icon_resize_circle_fill_black">
+                </div>
+                <div class="col">
+                    <h5> About </h5>
+                </div>
+            </div>
+        </section>
+        <section class="col nav_link_wa_center div_class_style_4" >
+            <div class="container-fluid div_class_style_4">
+                <div>
+                    <img src="wa_picture/cover1.png" alt ="artist cover" >
+                </div>
+            </div>
+            <div class="container-fluid div_class_style_3">
+                <div class="row">
+                    <div class="col">
+                        <h3> Cage the Elephant</h3>
+                        <h6> Ready To Let Go</h6>
+                    </div>
+                    <div class="col">
+                        <img src="wa_icon/arrow_diagonal_up.png" alt = "see all icon" class="icon_resize_diagonal_center">
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="col div_class_style" >
+            <h5 class="text-end">Listeners</h5>
+            <ul class="nav flex-column">
+                <li class="nav-item cell_styles">
+                    <a class="nav-link nav_link_wa_right" href="#">
+                        <div class="row">
+                            <div class="col"> <h6>The Killer</h6> </div>
+                            <div class="col"> <h6 class="text-end">980,532 </h6> </div>
+                        </div>
+                    </a>
+                </li>
+                <li class="nav-item cell_styles">
+                    <a class="nav-link nav_link_wa_right" href="#">
+                        <div class="row">
+                            <div class="col"> <h6>Franz Ferdinand</h6> </div>
+                            <div class="col"> <h6 class="text-end">77,564 </h6> </div>
+                        </div>
+                    </a>
+                </li>
+                <li class="nav-item cell_styles">
+                    <a class="nav-link nav_link_wa_right" href="#">
+                        <div class="row">
+                            <div class="col"> <h6>Muze</h6> </div>
+                            <div class="col"> <h6 class="text-end">52,277 </h6> </div>
+                        </div>
+                    </a>
+                </li>
+                <li class="nav-item cell_styles">
+                    <a class="nav-link nav_link_wa_right" href="#">
+                        <div class="row">
+                            <div class="col"> <h6>MGTMT</h6> </div>
+                            <div class="col"> <h6 class="text-end">51,886 </h6> </div>
+                        </div>
+                    </a>
+                </li>
+                <li class="nav-item cell_styles">
+                    <a class="nav-link nav_link_wa_right" href="#">
+                        <div class="row">
+                            <div class="col"> <h6>The Strokes</h6> </div>
+                            <div class="col"> <h6 class="text-end">40,003 </h6> </div>
+                        </div>
+                    </a>
+                </li>
+                <li class="nav-item cell_styles">
+                    <a class="nav-link nav_link_wa_right_last" href="#">
+                        <div class="row">
+                            <div class="col"> <h6>Arctics Mokey</h6> </div>
+                            <div class="col"> <h6 class="text-end">38,092 </h6> </div>
+                        </div>
+                    </a>
+                </li>
+            </ul>
+            <div class="container-fluid div_class_style_3">
+                <div class="row">
+                    <div class="col">
+                        <a class="nav-link nav_link_wa_seall" href="#"> <h6>See All </h6> </a>
+                    </div>
+                    <div class="col">
+                            <img src="wa_icon/arrow_diagonal_up.png" alt = "see all icon" class="icon_resize_diagonal justify-content-end">
+                    </div>
+                </div>
+            </div>
+        </section>
+    </div>
+</section>
+</body>
+</html>
